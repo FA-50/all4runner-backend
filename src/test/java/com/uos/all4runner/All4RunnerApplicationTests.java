56333a4c8bde0e94a4bfeb24290df957f21f22ee
kf16 <wjdtn747@naver.com>
2025-12-18 23:30:59 +0900

    feat: 프로젝트 초기세팅

---
+++ src/test/java/com/uos/all4runner/All4RunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uos.all4runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class All4RunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
