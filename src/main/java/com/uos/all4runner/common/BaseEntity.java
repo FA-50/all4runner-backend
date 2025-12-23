46fb76ef3daf92c5e94350af4f01909f0749e222
kf16 <wjdtn747@naver.com>
2025-12-23 17:13:02 +0900

    feat: AccountService 리팩터링 # 2

--- src/main/java/com/uos/all4runner/common/BaseEntity.java
+++ src/main/java/com/uos/all4runner/common/BaseEntity.java
@@ -6,14 +6,19 @@ import java.util.UUID;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
+@Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 	@Id
 	@UuidGenerator
