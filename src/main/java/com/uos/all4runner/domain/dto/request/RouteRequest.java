9cd59e91ab4cf0c23515fdf8f8617234ce62c514
kf16 <wjdtn747@naver.com>
2025-12-29 14:25:53 +0900

    feat : Route 생성 기능추가

--- src/main/java/com/uos/all4runner/domain/dto/request/RouteRequest.java
+++ src/main/java/com/uos/all4runner/domain/dto/request/RouteRequest.java
@@ -7,21 +7,30 @@ import com.uos.all4runner.constant.LinkType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public interface RouteRequest {
-	@Schema(name = "RouteRequest.ShortPath")
-	record ShortPath(
+	@Schema(name = "RouteRequest.ShortRoute")
+	record ShortRoute(
 		@NotBlank String nodeIdSet,
 		@Min(value = 0) int maxDistance,
-		@NotBlank LinkType excludeType
+		@NotNull LinkType excludeType
 		){
 	}
-	@Schema(name = "RouteRequest.OptimalPath")
-	record OptimalPath(
+
+	@Schema(name = "RouteRequest.OptimalRoute")
+	record OptimalRoute(
 		@NotBlank String nodeIdSet,
 		@Min(value = 0) int maxDistance,
 		@Range(min = 0, max = 20, message = "경사도는 0~20도 범위 이내로 지정해야합니다.") int slopeConstraints,
-		@NotBlank LinkType excludeType
+		@NotNull LinkType excludeType
 	){
+	}
+
+	@Schema(name = "RouteRequest.TempToPublic")
+	record TempToPublic(
+		String description,
+		@NotBlank String categoryName
+		){
 	}
 }
