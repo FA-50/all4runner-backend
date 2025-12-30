2540cea57b462d04af5b585fce3d6baac6f833b9
kf16 <wjdtn747@naver.com>
2025-12-30 13:10:54 +0900

    feat : RouteRepository 임시 경로 삭제기능 추가

--- src/main/java/com/uos/all4runner/repository/route/RouteRepository.java
+++ src/main/java/com/uos/all4runner/repository/route/RouteRepository.java
@@ -3,7 +3,10 @@ package com.uos.all4runner.repository.route;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.uos.all4runner.constant.ErrorCode;
 import com.uos.all4runner.domain.entity.route.Route;
@@ -16,4 +19,13 @@ public interface RouteRepository extends JpaRepository<Route, UUID> {
 			()-> new CustomException(ErrorCode.ROUTE_NOT_FOUND)
 		);
 	}
+
+	@Modifying
+	@Transactional
+	@Query("""
+		delete from Route r
+				where r.account.id = ?1
+				and r.routeStatus = 'TEMP'
+		""")
+	void deleteTempByAccountId(UUID accountId);
 }
